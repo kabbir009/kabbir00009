6656e71c0e5f1f993a537387c49e389749b63432
kabbir009 <98680308+kabbir009@users.noreply.github.com>
2022-07-03 11:00:29 +0600

    Add files via upload

---
+++ Q2.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>
+                <h1>Tutuor Information</h1>
+            </th>
+
+        </tr>
+
+
+
+        <th>
+        <td>
+            <label for="fname">First Name</label>
+            <input type="text">
+
+            <br>
+
+            <label for="fname">Last Name</label>
+            <input type="text">
+            <br>
+            <label for="fname">University</label>
+            <input type="text">
+            <br>
+            <label for="fname">Department</label>
+            <input type="text">
+            <br>
+            <label for="fname">Year</label>
+            <input type="text">
+            <br>
+            <label for="fname">Address</label>
+            <input type="text">
+            <br>
+            <label for="state">Exam</label>
+            <select name="state" id="state">
+                <option value="null">Select a Exam</option>
+                <option value="null">S.S.C/O level</option>
+                <option value="null">H.S.C/A level</option>
+            </select>
+            <br>
+            <label for="zcode">Result</label>
+            <input type="text">
+            <br>
+            <label for="state">Exam</label>
+            <select name="state" id="state">
+                <option value="null">Select a Exam</option>
+                <option value="null">S.S.C/O level</option>
+                <option value="null">H.S.C/A level</option>
+            </select>
+            <br>
+            <label for="zcode">Result</label>
+            <input type="text">
+            <br>
+            <label for="fname">Phone</label>
+            <input type="text">
+            <br>
+            <label for="fname">E-mail</label>
+            <input type="text">
+            <br>
+            <label for="fname">Fax</label>
+            <input type="text">
+            <br>
+            <label for="Damount">Donation Amount</label>
+            <input type="radio" id="none" name="damount" value="none">
+            <label for="none">None</label>
+            <input type="radio" id="50" name="damount" value="50">
+            <label for="50">$50</label>
+            <input type="radio" id="100" name="damount" value="100">
+            <label for="100">$100</label>
+            <input type="radio" id="others" name="damount" value="others">
+            <label for="100">$Others</label>
+            <br>
+        </td>
+        </th>
+</body>
+
